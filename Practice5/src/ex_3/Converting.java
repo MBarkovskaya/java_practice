8a03c1e4bba4f9cffcb460ade05a01586e5676ff
Mariya Barkovskaya <mariya.barkovskaya@nosota.com>
2019-04-12 16:16:02 +0300

    pr5

--- Practice5/src/ex_3/Converting.java
+++ Practice5/src/ex_3/Converting.java
@@ -1,41 +1,23 @@
 package ex_3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Converting {
     public static void main(String[] args) {
-        convert(Collections.singletonList("When I was younger"));
-        convert(Collections.singletonList("I never needed"));
-//        convert(getInput(2));
+        convert1();
     }
 
-    public static void convert(List<String> input) {
-        for (int i = 0; i < input.size(); i++) {
-            Pattern pattern = Pattern.compile("(U)^[a-z]");
-            Matcher m = Pattern.compile("(?U)(\\w)(\\w+)").matcher(input.toString());
-            StringBuffer sb = new StringBuffer();
-            while (m.find()) {
-                m.appendReplacement(sb, m.group(1).toUpperCase() + m.group(2));
-            }
-            m.appendTail(sb);
-            System.out.println(sb.toString());
+    public static void convert1() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a Sentence: ");
+        String line = in.nextLine();
+        String upper_case_line = "";
+        Scanner lineScan = new Scanner(line);
+        while (lineScan.hasNext()) {
+            String word = lineScan.next();
+            upper_case_line += Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
         }
-
-    }
-
-    private static ArrayList<String> getInput(int num) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            System.out.println("Write text: ");
-            list.add(sc.nextLine());
-        }
-        return list;
+        System.out.println(upper_case_line.trim());
     }
 
 }
